125394d6e791cdc7d08b907bba0380563664d898
AddSu <rauladsuara00@gmail.com>
2025-08-11 22:26:32 +0200

    fix: implemented new structur for project example

--- app/projects/impl/project2/Project2.tsx
+++ app/projects/impl/project2/Project2.tsx
@@ -1,13 +1,27 @@
-// projects/Project1.ts
+// projects/Project2.ts
 
 import { Project } from '../../Project';
+import React from 'react';
 
 export class Project2 implements Project {
   id = 'proj2';
   name = 'Proyecto 2';
   descriptionPath = 'proj2.md';
 
-  action(input: string): string {
-    return `Proyecto 2 process: ${input}`;
+  async action(input: string): Promise<any> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`Proyecto 2 process: ${input}`);
+      }, 500);
+    });
+  }
+
+  renderResult(result: any): React.ReactNode {
+    return (
+      <div>
+        <h2>{this.name}</h2>
+        <p>Resultado: {result}</p>
+      </div>
+    );
   }
 }
