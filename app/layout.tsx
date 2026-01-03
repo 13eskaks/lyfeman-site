3b2af3126d574924d0ccd8a5e5de36920ffd31c5
Raúl Adsuara <147850763+13eskaks@users.noreply.github.com>
2026-01-03 16:37:56 +0100

    Include Google Ads script in RootLayout
    Added Google Ads script to the head section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5145520648111228"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="bg-gray-900 text-white font-sans">{children}</body>
     </html>
   );
